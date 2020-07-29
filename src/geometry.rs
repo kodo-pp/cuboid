7c6012c09aa983a2372728c704dabbc2de4df996
Alexander Korzun <sahhash33@gmail.com>
2020-07-29 20:13:16 +0300

    Optimize triangle filling

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,5 +1,7 @@
 use sdl2::rect::Rect;
 use std::ops::{Sub, Add, Neg};
+use std::mem;
+use gcd::Gcd;
 
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
@@ -117,5 +119,138 @@ impl Triangle {
         let vec_bc = self.c - self.b;
         let vec_ca = self.a - self.c;
         equal3(vec_ab.orientation(vec_ap), vec_bc.orientation(vec_bp), vec_ca.orientation(vec_cp))
+    }
+}
+
+
+#[derive(Debug, Clone, Copy)]
+pub struct Line {
+    a: i32,
+    b: i32,
+    c: i32,
+}
+
+impl Line {
+    pub fn from_points(p: Point, q: Point) -> Line {
+        if p == q {
+            panic!("More than one line passes through two coinciding points");
+        }
+
+        if p.x == q.x {
+            Line { a: 1, b: 0, c: -p.x }
+        } else {
+            Line { a: p.y - q.y, b: q.x - p.x, c: p.x * q.y - p.y * q.x }
+        }
+    }
+
+    pub fn horizontal(y: i32) -> Line {
+        Line { a: 0, b: 1, c: -y }
+    }
+
+    pub fn intersect(self, other: Line) -> Point {
+        let x_numerator = self.c * other.b - self.b * other.c;
+        let y_numerator = self.a * other.c - self.c * other.a;
+        let denominator = self.b * other.a - self.a * other.b;
+        Point { x: x_numerator / denominator, y: y_numerator / denominator }
+    }
+}
+
+impl PartialEq for Line {
+    fn eq(&self, other: &Line) -> bool {
+        let ga = (self.a.abs() as u32).gcd(other.a.abs() as u32) as i32;
+        let p = (self.a / ga) as i64;
+        let q = (other.a / ga) as i64;
+        
+        let  first = self.a as i64 * q == other.a as i64 * p;
+        let second = self.b as i64 * q == other.b as i64 * p;
+        let  third = self.c as i64 * q == other.c as i64 * p;
+        first && second && third
+    }
+}
+
+impl Eq for Line {}
+
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub struct HorizontalSegment {
+    left: Point,
+    width: u32,
+}
+
+impl HorizontalSegment {
+    pub fn from_points(mut left: Point, mut right: Point) -> HorizontalSegment {
+        assert_eq!(left.y, right.y, "Y coordinates of points of a horizontal segment must coincide");
+        if left.x > right.x {
+            mem::swap(&mut left, &mut right);
+        }
+        HorizontalSegment { left, width: (right.x - left.x) as u32 }
+    }
+
+    pub fn left(self) -> Point {
+        self.left
+    }
+
+    pub fn right(self) -> Point {
+        Point { x: self.left.x + (self.width as i32), y: self.left.y }
+    }
+
+    pub fn width(self) -> u32 {
+        self.width
+    }
+
+    pub fn y(self) -> i32 {
+        self.left.y
+    }
+}
+
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub struct GluedTriangle {
+    pub horizontal_segment: HorizontalSegment,
+    pub free_point: Point,
+}
+
+impl GluedTriangle {
+    pub fn new(horizontal_segment: HorizontalSegment, free_point: Point) -> GluedTriangle {
+        GluedTriangle { horizontal_segment, free_point }
+    }
+}
+
+
+pub trait Triangular {
+    fn points(&self) -> (Point, Point, Point);
+
+    fn sort_points<K: Ord>(&self, key: impl Fn(Point) -> K) -> (Point, Point, Point) {
+        let (mut a, mut b, mut c) = self.points();
+        if key(a) > key(b) {
+            mem::swap(&mut a, &mut b);
+        }
+        if key(b) > key(c) {
+            mem::swap(&mut b, &mut c);
+        }
+        if key(a) > key(b) {
+            mem::swap(&mut a, &mut b);
+        }
+        (a, b, c)
+    }
+
+    fn xsort(&self) -> (Point, Point, Point) {
+        self.sort_points(|p| p.x)
+    }
+
+    fn ysort(&self) -> (Point, Point, Point) {
+        self.sort_points(|p| p.y)
+    }
+}
+
+impl Triangular for Triangle {
+    fn points(&self) -> (Point, Point, Point) {
+        (self.a, self.b, self.c)
+    }
+}
+
+impl Triangular for GluedTriangle {
+    fn points(&self) -> (Point, Point, Point) {
+        (self.free_point, self.horizontal_segment.left(), self.horizontal_segment.right())
     }
 }
--- src/render.rs
+++ src/render.rs
@@ -1,9 +1,10 @@
-use crate::geometry::{Point, Triangle};
+use crate::geometry::{Point, Triangle, Line, HorizontalSegment, GluedTriangle, Triangular};
+use super::SdlError;
 
 use sdl2::pixels::Color;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::video::WindowSurfaceRef;
-use super::SdlError;
+use std::mem;
 
 pub struct Renderer {}
 
@@ -36,9 +37,21 @@ impl Renderer {
     fn render_pixels<'a>(context: &mut RenderContext<'a>) {
         let a = Point {x: 50, y: 100};
         let b = Point {x: 200, y: 500};
-        let c = Point {x: 800, y: 400};
+        let c = Point {x: 700, y: 400};
         let tri = Triangle {a, b, c};
-        context.fill_triangle(&tri, RGB {r: 255, g: 0, b: 0});
+        context.fill_triangle(tri, RGB {r: 255, g: 0, b: 0});
+        /*
+        context.fill_glued_triangle(
+            GluedTriangle {
+                horizontal_segment: HorizontalSegment::from_points(
+                    Point {x: 100, y: 100},
+                    Point {x: 700, y: 100}
+                ),
+                free_point: Point {x: 600, y: 500},
+            },
+            RGB {r: 255, g: 0, b: 0}
+        );
+        */
     }
 }
 
@@ -58,6 +71,7 @@ struct RenderContext<'a> {
 }
 
 impl RenderContext<'_> {
+    #[inline]
     pub fn set(&mut self, x: u32, y: u32, value: RGB) {
         self.data[self.index_at(x, y, 0)] = value.b;
         self.data[self.index_at(x, y, 1)] = value.g;
@@ -70,13 +84,35 @@ impl RenderContext<'_> {
         ((self.width * y + x) * 4 + component) as usize
     }
 
-    pub fn fill_triangle(&mut self, tri: &Triangle, value: RGB) {
-        let rect = tri.bounding_rect();
-        for y in rect.top()..rect.bottom() {
-            for x in rect.left()..rect.right() {
-                if tri.contains(Point{x, y}) {
-                    self.set(x as u32, y as u32, value);
-                }
+    pub fn fill_triangle(&mut self, tri: Triangle, value: RGB) {
+        let (a, b, c) = tri.ysort();
+        let line_hb = Line::horizontal(b.y);
+        let line_ac = Line::from_points(a, c);
+        let split_point = line_hb.intersect(line_ac);
+        let horizontal_segment = HorizontalSegment::from_points(b, split_point);
+        let glued_top = GluedTriangle::new(horizontal_segment, a);
+        let glued_bottom = GluedTriangle::new(horizontal_segment, c);
+        self.fill_glued_triangle(glued_top, value);
+        self.fill_glued_triangle(glued_bottom, value);
+    }
+
+    fn fill_glued_triangle(&mut self, glued_tri: GluedTriangle, value: RGB) {
+        let mut min = glued_tri.horizontal_segment.y();
+        let mut max = glued_tri.free_point.y;
+        if min > max {
+            mem::swap(&mut min, &mut max);
+        }
+
+        let  left_line = Line::from_points(glued_tri.horizontal_segment.left(),  glued_tri.free_point);
+        let right_line = Line::from_points(glued_tri.horizontal_segment.right(), glued_tri.free_point);
+
+        for y in min..max {
+            let horizontal_line = Line::horizontal(y);
+            let  left_isect = horizontal_line.intersect(left_line);
+            let right_isect = horizontal_line.intersect(right_line);
+
+            for x in (left_isect.x)..(right_isect.x) {
+                self.set(x as u32, y as u32, value);
             }
         }
     }
